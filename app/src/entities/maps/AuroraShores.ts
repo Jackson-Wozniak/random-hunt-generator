1fd0eb0d5209e13df290b1821789a61d3e7a2a5a
Jackson Wozniak <jjw30@comcast.net>
2025-09-15 22:44:54 -0400

    add all aurora shores regions

--- app/src/entities/maps/AuroraShores.ts
+++ app/src/entities/maps/AuroraShores.ts
@@ -1,5 +1,11 @@
 import { MapName, type Map } from "../../types/entities/Map";
 import { type Region, Biome } from "../../types/entities/Region";
+import { AmericanBlackBear, KodiakBear } from "../species/Bears";
+import { GrayWolf, RedFox } from "../species/Canines";
+import { AlaskaMoose, BarrenGroundCaribou, RooseveltElk, SitkaDeer } from "../species/Deer";
+import { SnowshoeHare } from "../species/Rabbits";
+import { LesserScaup, SurfScoter, WildDuck } from "../species/WaterBirds";
+import { MountainGoat, WoodBison } from "../species/WildCattle";
 
 export const NianukRise: Region = {
     name: "Nianuk Rise",
@@ -9,7 +15,122 @@ export const NianukRise: Region = {
     campsiteCount: 1,
     huntingStandCount: 0,
     species: [
-        
+        SnowshoeHare, RedFox, MountainGoat, GrayWolf, BarrenGroundCaribou
+    ]
+}
+
+export const ShalaskaPlains: Region = {
+    name: "Shalaska Plains",
+    biome: Biome.GRASSLAND,
+    hasCabin: false,
+    isPrivatePass: false,
+    campsiteCount: 1,
+    huntingStandCount: 0,
+    species: [
+        AlaskaMoose, AmericanBlackBear, BarrenGroundCaribou, GrayWolf,
+        LesserScaup, RedFox, RooseveltElk, SnowshoeHare, WoodBison
+    ]
+}
+
+export const PlymbaltRange: Region = {
+    name: "PlymbaltRange",
+    biome: Biome.HIGHLANDS,
+    hasCabin: false,
+    isPrivatePass: false,
+    campsiteCount: 1,
+    huntingStandCount: 0,
+    species: [
+        RooseveltElk, SitkaDeer, SnowshoeHare
+    ]
+}
+
+export const AntlerHighlands: Region = {
+    name: "Antler Highlands",
+    biome: Biome.HIGHLAND_FOREST,
+    hasCabin: false,
+    isPrivatePass: false,
+    campsiteCount: 1,
+    huntingStandCount: 0,
+    species: [
+        AlaskaMoose, AmericanBlackBear, BarrenGroundCaribou, GrayWolf,
+        RedFox, RooseveltElk, SitkaDeer, SnowshoeHare, WoodBison
+    ]
+}
+
+export const PointWritter: Region = {
+    name: "PointWritter",
+    biome: Biome.MOUNTAINS,
+    hasCabin: false,
+    isPrivatePass: false,
+    campsiteCount: 1,
+    huntingStandCount: 0,
+    species: [
+        AlaskaMoose, BarrenGroundCaribou, GrayWolf, MountainGoat
+    ]
+}
+
+export const LongnoseTips: Region = {
+    name: "Longnose Tips",
+    biome: Biome.HIGHLAND_FOREST,
+    hasCabin: false,
+    isPrivatePass: false,
+    campsiteCount: 1,
+    huntingStandCount: 0,
+    species: [
+        AlaskaMoose, AmericanBlackBear, BarrenGroundCaribou, GrayWolf,
+        RedFox, RooseveltElk, SitkaDeer, SnowshoeHare, WoodBison
+    ]
+}
+
+export const FigwortTails: Region = {
+    name: "Figwort Tails",
+    biome: Biome.GRASSLAND,
+    hasCabin: false,
+    isPrivatePass: false,
+    campsiteCount: 1,
+    huntingStandCount: 0,
+    species: [
+        AlaskaMoose, AmericanBlackBear, BarrenGroundCaribou, GrayWolf,
+        LesserScaup, RedFox, RooseveltElk, SnowshoeHare,
+        SurfScoter, WildDuck, WoodBison
+    ]
+}
+
+export const GraylingPeaks: Region = {
+    name: "Grayling Peaks",
+    biome: Biome.HIGHLAND_FOREST,
+    hasCabin: false,
+    isPrivatePass: false,
+    campsiteCount: 1,
+    huntingStandCount: 0,
+    species: [
+        AlaskaMoose, AmericanBlackBear, BarrenGroundCaribou, GrayWolf,
+        RedFox, RooseveltElk, SnowshoeHare, WildDuck, WoodBison
+    ]
+}
+
+export const RustowikGrasslandss: Region = {
+    name: "Rustowik Grasslands",
+    biome: Biome.GRASSLAND,
+    hasCabin: false,
+    isPrivatePass: false,
+    campsiteCount: 1,
+    huntingStandCount: 0,
+    species: [
+        AlaskaMoose, AmericanBlackBear, BarrenGroundCaribou, GrayWolf,
+        LesserScaup, RedFox, RooseveltElk, SnowshoeHare, SurfScoter, WildDuck, WoodBison
+    ]
+}
+
+export const AivukRainforest: Region = {
+    name: "Aivuk Rainforest",
+    biome: Biome.RAINFOREST,
+    hasCabin: true,
+    isPrivatePass: true,
+    campsiteCount: 2,
+    huntingStandCount: 0,
+    species: [
+        KodiakBear, LesserScaup, RooseveltElk, SitkaDeer, SurfScoter
     ]
 }
 
@@ -17,6 +138,8 @@ export const AuroraShores: Map = {
     name: MapName.AURORA_SHORES,
     isDLC: true,
     regions: [
-        NianukRise
+        NianukRise, ShalaskaPlains, PlymbaltRange, AntlerHighlands,
+        PointWritter, LongnoseTips, FigwortTails, GraylingPeaks,
+        RustowikGrasslandss, AivukRainforest
     ]
 }
